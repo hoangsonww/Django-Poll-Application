472090d9bebd080f4a25a32a97e48cc1603e8865
Son Nguyen <hoangson091104@gmail.com>
2024-06-12 10:13:40 +0700

    Initial Commit

--- PollApplication/settings.py
+++ PollApplication/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qx14ogui1^h3@h8zl38-8@3t%n8y0r_-anx$26=!pecc#=gx#^'
+# SECRET_KEY = 'your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
