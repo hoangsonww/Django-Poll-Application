3c42a66ba6a6bf0692c28d4e4d98fce855069256
Son Nguyen <hoangson091104@gmail.com>
2024-09-01 10:16:01 -0400

    Final: App Working

--- PollApplication/settings.py
+++ PollApplication/settings.py
@@ -19,10 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
-
 ALLOWED_HOSTS = []
 
 
