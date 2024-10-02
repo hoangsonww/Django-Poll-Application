c5059dc6c4681af00ab85ff4363f63448a3e85d0
Son Nguyen <hoangson091104@gmail.com>
2024-10-02 18:35:54 -0400

    Final: App Working

--- polls/models.py
+++ polls/models.py
@@ -12,6 +12,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+
     @admin.display(
         boolean=True,
         ordering="pub_date",
@@ -27,6 +28,7 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     is_correct = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.choice_text
